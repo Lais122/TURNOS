181d19023e0202f80c77c738387a9936fcb997bc
Laís Pinheiro <laisnathally30@gmail.com>
2025-10-03 15:11:19 -0300

    sw.java

---
+++ sw.java
@@ -0,0 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("app-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/pag.html",
+        "/index.css",
+        "/pag.css",
+        "/lais.jpeg"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
